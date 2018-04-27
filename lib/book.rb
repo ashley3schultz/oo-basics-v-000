e282ba2e9ba055111ad0e519d6e68105ced89ae2
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-27 03:04:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :title, :author, :page_count, :genre
   def initialize(title)
     @title = title
   end
   
-  def title 
-    @title
-  end
-  
+
 end
 
