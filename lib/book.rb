39da421f1df5bb2bf54a1f388b77e6cc26bbae71
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-27 03:11:10 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  
   def initialize(title)
     @title = title
   end
@@ -8,6 +9,5 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
 
